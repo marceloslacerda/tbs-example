15c992997d8e7eaa9d0b7c3797c5627835cff306
Marcelo Lacerda <marceloslacerda@gmail.com>
2018-04-08 14:03:31 -0300

    Fix #5

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,4 @@
-import sys
+import os
 
 import conz
 from fabric.api import local
@@ -8,19 +8,19 @@ cn = conz.Console(verbose=True)
 
 
 def run():
-    if 'TOKEN' not in sys.argv:
+    if 'TOKEN' not in os.environ:
         token = cn.rvpl('Telegram token not set in the environment variable,'
                         ' please input one:')
     else:
-        token = sys.argv['TOKEN']
-    if 'DEFAULT_CHANNEL' not in sys.argv:
+        token = os.environ['TOKEN']
+    if 'DEFAULT_CHANNEL' not in os.environ:
         yes = cn.yesno('Do you want to specify a default channel?')
         if yes:
             default_channel = cn.rvpl('Default channel:')
         else:
             default_channel = '999999'
     else:
-        default_channel = sys.argv['DEFAULT_CHANNEL']
+        default_channel = os.environ['DEFAULT_CHANNEL']
     with shell_env(
             TOKEN=token,
             DEFAULT_CHANNEL=default_channel
